2360b974ec42bca9829f3dbb07c9962efce0c6da
Tim Dettmers <tim.dettmers@gmail.com>
2017-04-26 12:47:56 +0100

    Fixed CUDATimer issues.

--- spodernet/utils/util.py
+++ spodernet/utils/util.py
@@ -3,6 +3,7 @@ from os.path import join
 import h5py
 import os
 import time
+import torch
 
 from spodernet.utils.logger import Logger
 from torch.cuda import Event
@@ -81,19 +82,20 @@ class CUDATimer(object):
         self.cumulative_secs = {}
         self.current_ticks = {}
         self.silent = silent
-        self.start = Event(enable_timing=True, blocking=True)
         self.end = Event(enable_timing=True, blocking=True)
 
     def tick(self, name='default'):
         if name not in self.current_ticks:
-            self.start.record()
-            self.current_ticks[name] = self.start
+            start = Event(enable_timing=True, blocking=True)
+            start.record()
+            self.current_ticks[name] = start
 
             return 0.0
         else:
             if name not in self.cumulative_secs:
                 self.cumulative_secs[name] = 0
             self.end.record()
+            self.end.synchronize()
             self.cumulative_secs[name] += self.current_ticks[name].elapsed_time(self.end)/1000.
             self.current_ticks.pop(name)
 
@@ -105,6 +107,8 @@ class CUDATimer(object):
         if not self.silent:
             log.info('Time taken for {0}: {1:.8f}s'.format(name, value))
         self.cumulative_secs.pop(name)
+        if name in self.current_ticks:
+            del self.current_ticks[name]
         self.current_ticks.pop(name, None)
 
         return value
--- tests/test_torch.py
+++ tests/test_torch.py
@@ -9,7 +9,7 @@ from spodernet.utils.util import CUDATimer
 def test_cuda_timer():
     start = torch.cuda.Event(enable_timing=True, blocking=True)
     end = torch.cuda.Event(enable_timing=True, blocking=True)
-    k = 100
+    k = 1000
     a = torch.rand(k,k).cuda()
     b = torch.rand(k,k).cuda()
     timer = CUDATimer()
@@ -17,12 +17,23 @@ def test_cuda_timer():
     t0 = time.time()
     start.record()
     timer.tick('a')
-    for i in range(1000):
+    timer.tick('timers')
+    for i in range(10000):
+        timer.tick('b')
+        timer.tick('timers')
         c = a*b
+        timer.tick('timers')
+        timer.tick('b')
+    torch.cuda.synchronize()
     end.record()
+    overhead = timer.tock('timers')
     timer_time = timer.tock('a')
+    timer_time_cumulative = timer.tock('b') + overhead
     cpu_time = time.time() - t0
-    cuda_time = start.elapsed_time(end)/1000.
+    cuda_time = (start.elapsed_time(end)/1000.)
+
+    print(cpu_time, cuda_time, timer_time, timer_time_cumulative)
 
     assert timer_time > cpu_time*0.9 and timer_time < cpu_time*1.1, 'CUDATimer imprecise with respect to CPU time.'
     assert timer_time > cuda_time*0.9 and timer_time < cuda_time*1.1, 'CUDATimer imprecise with respect to CUDA time.'
+    assert timer_time_cumulative > cuda_time*0.9 and timer_time_cumulative < cuda_time*1.1, 'Cumulative CUDATimer imprecise with respect to CUDA time.'
