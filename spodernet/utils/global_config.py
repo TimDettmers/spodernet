dfc5524c8849e448bce4801bd5048d6d757aec9a
Tim Dettmers <tim.dettmers@gmail.com>
2017-05-04 13:12:46 +0100

    Added learning rate decay and optimizer to global config.

--- spodernet/utils/global_config.py
+++ spodernet/utils/global_config.py
@@ -22,6 +22,8 @@ class Config:
     feature_map_dropout = 0.0
     use_conv_transpose = False
     use_bias = True
+    optimizer = 'adam'
+    learning_rate_decay = 0
 
     @staticmethod
     def parse_argv(argv):
@@ -60,6 +62,7 @@ class Config:
 
 params2type = {}
 params2type['learning_rate'] = lambda x: float(x)
+params2type['learning_rate_decay'] = lambda x: float(x)
 params2type['dropout'] = lambda x: float(x)
 params2type['batch_size'] = lambda x: int(x)
 params2type['L2'] = lambda x: float(x)
@@ -69,19 +72,23 @@ params2type['input_dropout'] = lambda x: float(x)
 params2type['feature_map_dropout'] = lambda x: float(x)
 params2type['use_conv_transpose'] = lambda x: x.lower() == 'true' or x == '1'
 params2type['use_bias'] = lambda x: x.lower() == 'true' or x == '1'
+params2type['optimizer'] = lambda x: x
 
 alias2params = {}
 alias2params['lr'] = 'learning_rate'
+alias2params['lr_decay'] = 'learning_rate_decay'
 alias2params['l2'] = 'L2'
 alias2params['input_drop'] = 'input_dropout'
 alias2params['hidden_drop'] = 'dropout'
 alias2params['feat_drop'] = 'feature_map_dropout'
 alias2params['bias'] = 'use_bias'
 alias2params['conv_trans'] = 'use_conv_transpose'
+alias2params['opt'] = 'optimizer'
 
 
 params2field = {}
 params2field['learning_rate'] = lambda x: setattr(Config, 'learning_rate', x)
+params2field['learning_rate_decay'] = lambda x: setattr(Config, 'learning_rate_decay', x)
 params2field['dropout'] = lambda x: setattr(Config, 'dropout', x)
 params2field['batch_size'] = lambda x: setattr(Config, 'batch_size', x)
 params2field['L2'] = lambda x: setattr(Config, 'L2', x)
@@ -91,5 +98,6 @@ params2field['input_dropout'] = lambda x: setattr(Config, 'input_dropout', x)
 params2field['feature_map_dropout'] = lambda x: setattr(Config, 'feature_map_dropout', x)
 params2field['use_conv_transpose'] = lambda x: setattr(Config, 'use_conv_transpose', x)
 params2field['use_bias'] = lambda x: setattr(Config, 'use_bias', x)
+params2field['optimizer'] = lambda x: setattr(Config, 'optimizer', x)
 
 
